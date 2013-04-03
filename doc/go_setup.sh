15bbfc5ce698fa31a5beba9d637861a5460e56b0
suhas kodgire <suhas.kodgire@gslab.com>
2013-04-03 18:07:47 +0530

    setup file for the go language and go_api_sample

---
+++ doc/go_setup.sh
@@ -0,0 +1,13 @@
+sudo apt-get install gcc libc6-dev
+sudo apt-get install mercurial
+hg clone -u release https://code.google.com/p/go
+cd go/src
+./all.bash
+sudo cp go/bin/* /usr/local/bin/
+go get github.com/ziutek/mymysql/thrsafe
+go get github.com/ziutek/mymysql/autorc
+go get github.com/ziutek/mymysql/godrv
+go get github.com/pelletier/go-toml
+go get labix.org/v2/mgo
+go get labix.org/v2/mgo/bson
+
