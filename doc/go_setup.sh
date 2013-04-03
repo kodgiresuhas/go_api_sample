b42c431ed2d9d48108f5168f0bc73d96c6633245
Suhas Kodgire <kodgiresuhas@gmail.com>
2013-04-03 18:12:47 +0530

    Update go_setup.sh

--- doc/go_setup.sh
+++ doc/go_setup.sh
@@ -3,7 +3,7 @@ sudo apt-get install mercurial
 hg clone -u release https://code.google.com/p/go
 cd go/src
 ./all.bash
-sudo cp go/bin/* /usr/local/bin/
+sudo cp ~/go/bin/* /usr/local/bin/
 go get github.com/ziutek/mymysql/thrsafe
 go get github.com/ziutek/mymysql/autorc
 go get github.com/ziutek/mymysql/godrv
